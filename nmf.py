87136cfd06e57120f338b789a0b281ae24adbc35
Philipp Schreitmueller <philipp.schreitmueller@in.tum.de>
2014-11-09 01:58:32 +0000

    Working input of raw data.

--- nmf.py
+++ nmf.py
@@ -1,2 +1,24 @@
 __author__ = 'Philipp'
 
+import numpy as np
+
+
+def readData():
+    f = open("data/bbcnews.mtx")
+    counter = 0
+    for line in f.readlines():
+        if counter == 1:
+            debug = tuple([int(elem) for elem in line.split(" ", 2)[:2]])
+            ret = np.zeros(debug, dtype=float)
+        else:
+            if counter > 1:
+                vals = line.replace("\n", "").split(' ', 2)
+                x, y = map(int, vals[:2])
+                z = float(vals[2])
+                ret[x-1, y-1] = z
+        counter += 1
+    f.close()
+    return ret
+
+
+print(readData().shape)
